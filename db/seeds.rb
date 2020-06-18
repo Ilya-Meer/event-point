8a63133a44e460839ea5bef9e4e2d52fbd34a2ef
Ilya Meerovich <ilya.meerov@gmail.com>
2020-06-18 16:56:27 -0700

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Seed db with Users
+5.times do
+  display_name = Faker::Name.first_name
+  User.create(display_name: display_name)
+end
+
+# Seed db with Events
+Event.create(
+    topic: "Docker",
+    description: "A primer on how to get up and running with Docker",
+    datetime: DateTime.new(2021,2,3,4,5,6)
+)
