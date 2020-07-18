70c0de050b6586ad6fd03258299d032ef910c9ca
Ilya Meerovich <ilya.meerov@gmail.com>
2020-07-17 22:35:00 -0700

    Add authentication mechanism

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::SessionsController < ApplicationController
+    include CurrentUserConcern
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            render json: {
+                status: :ok,
+                logged_in: true,
+                user: user
+            }
+        else
+            head :unauthorized
+        end
+    end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def logout
+        reset_session
+        render json: {
+            status: :ok,
+            logged_out: true
+        }
+    end
+end
