0200e2d05bf704db7d1c5a9254afd0f27bcb0d18
Ilya Meerovich <ilya.meerov@gmail.com>
2020-07-18 22:27:26 -0700

    Remove votes column from events

---
+++ db/migrate/20200719052609_remove_votes_from_events.rb
@@ -0,0 +1,5 @@
+class RemoveVotesFromEvents < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :events, :votes, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_163113) do
+ActiveRecord::Schema.define(version: 2020_07_19_052609) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "topic"
     t.text "description"
-    t.integer "votes"
     t.datetime "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
