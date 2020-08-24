89ed8ac1caa367de9b86deb8a5700b631b764532
Ilya Meerovich <ilya.meerov@gmail.com>
2020-08-23 23:09:33 -0700

    Add Vote Controller test

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should add a vote" do
+    assert_equal(Event.first.votes.count, 1)
+    
+    post("/api/v1/add_vote", params: {
+      vote: {
+        user_id: 2,
+        event_id: Event.first.id
+      }
+    })
+
+    assert_response :success
+
+    assert_equal(Event.first.votes.count, 2)
+  end
+
+  test "should remove a vote" do
+    assert_equal(Event.first.votes.count, 1)
+    
+    post("/api/v1/remove_vote", params: {
+      user_id: 1,
+      event_id: Event.first.id
+    })
+
+    assert_response :success
+
+    assert_equal(Event.first.votes.count, 0)
+  end
 end
