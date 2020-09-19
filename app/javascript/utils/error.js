d32283677e6f77f02fc3dc8f0992ee6b42e42868
Ilya Meerovich <ilya.meerov@gmail.com>
2020-09-19 00:05:06 -0700

    Add login / register errors to flash message

--- app/javascript/utils/error.js
+++ app/javascript/utils/error.js
@@ -1,6 +1,9 @@
+const isSuccessfulResponse = (res) => res.status >= 200 && res.status < 300;
+
 const errorMessages = {
   login: 'Invalid login. Please try again.',
   register: 'Unable to register your account. Please try again.',
+  deleteEvent: 'Unable to delete event. Please try again.',
 };
 
-export { errorMessages };
+export { isSuccessfulResponse, errorMessages };
