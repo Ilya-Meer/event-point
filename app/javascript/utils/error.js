9c41c5b79e70f84d9c91cabb3494485e4e55f14c
Ilya Meerovich <ilya.meerov@gmail.com>
2020-09-19 13:31:25 -0700

    Add error handling for Schedule

--- app/javascript/utils/error.js
+++ app/javascript/utils/error.js
@@ -5,6 +5,9 @@ const errorMessages = {
   register: 'Unable to register your account. Please try again.',
   getEvents: 'Unable to fetch events. Please refresh the page to try again.',
   deleteEvent: 'Unable to delete event. Please try again.',
+  getSchedule:
+    'Unable to fetch schedule. Please refresh the page to try again.',
+  scheduleEvent: 'Unable to modify schedule. Please try again.',
   vote: 'Unable to register vote status. Please try again.',
 };
 
