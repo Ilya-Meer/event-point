9420b4cd67366f1f4a0e69454b9a061f88beda1f
Ilya Meerovich <ilya.meerov@gmail.com>
2020-09-19 12:29:43 -0700

    Add error handling for Event crud operations

--- app/javascript/utils/error.js
+++ app/javascript/utils/error.js
@@ -3,6 +3,7 @@ const isSuccessfulResponse = (res) => res.status >= 200 && res.status < 300;
 const errorMessages = {
   login: 'Invalid login. Please try again.',
   register: 'Unable to register your account. Please try again.',
+  getEvents: 'Unable to fetch events. Please refresh the page to try again.',
   deleteEvent: 'Unable to delete event. Please try again.',
 };
 
